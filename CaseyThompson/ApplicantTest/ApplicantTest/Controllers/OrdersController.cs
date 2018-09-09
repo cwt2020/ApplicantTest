ccbd63877204efd0f1b3cfcc4f0f9781288cc13e
Casey <cwt.2020@gmail.com>
2018-09-08 21:46:51 -0700

    OrdersList Completed

---
+++ CaseyThompson/ApplicantTest/ApplicantTest/Controllers/OrdersController.cs
@@ -0,0 +1,43 @@
+﻿using ApplicantTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+namespace ApplicantTest.Controllers
+{
+    public class OrdersController : Controller
+    {
+        // GET: Orders
+        public ActionResult Index()
+        {
+            List<Orders> orders = new List<Orders>();
+            string constr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                string query = "SELECT order_id, date_created, date_updated FROM `order`";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            orders.Add(new Orders
+                            {
+                                Order_Id = Convert.ToInt32(sdr["Order_Id"]),
+                                Date_Created = Convert.ToDateTime(sdr["Date_Created"]),
+                                Date_Updated = Convert.ToDateTime(sdr["Date_Updated"]),
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            return View(orders);
+        }
+    }
+}
---
+++ CaseyThompson/ApplicantTest/ApplicantTest/Models/Orders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApplicantTest.Models
+{
+    public class Orders
+    {
+        public int Order_Id{ get; set;}
+        public DateTime Date_Created { get; set; }
+        public DateTime Date_Updated { get; set; }
+    }
+}
