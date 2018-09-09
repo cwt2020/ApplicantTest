0f1868310e69b6be54d36bd51b1ff9858018e53f
Casey <cwt.2020@gmail.com>
2018-09-09 04:31:53 -0700

    Added Order View

--- CaseyThompson/ApplicantTest/ApplicantTest/Controllers/OrdersController.cs
+++ CaseyThompson/ApplicantTest/ApplicantTest/Controllers/OrdersController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using MySql.Data.MySqlClient;
 using System.Configuration;
+
 namespace ApplicantTest.Controllers
 {
     public class OrdersController : Controller
@@ -38,6 +39,67 @@ namespace ApplicantTest.Controllers
                 }
             }
             return View(orders);
+        }
+
+        // GET: Orders/Order/{order_id}
+        public ActionResult Order(int id)
+        {
+            var order = new Order();
+            order.Order_Id = id;
+            order.Items = new List<LineItem>();
+            order.Order_Total = 0.00m;
+            order.Order_Qty = 0;
+            string constr = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                string query = String.Format("SELECT * FROM applicant_test.`user` WHERE user_id = {0};",id);
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sdr.Read();
+                        order.User_FirstName = (sdr["User_FirstName"]).ToString();
+                        order.User_LastName = (sdr["User_LastName"]).ToString();
+                        order.User_Address1 = (sdr["User_Address1"]).ToString();
+                        order.User_Address2 = (sdr["User_Address2"]).ToString();
+                        order.User_City = (sdr["User_City"]).ToString();
+                        order.User_State = (sdr["User_State"]).ToString();
+                        order.User_Zip = (sdr["User_Zip"]).ToString();
+                    }
+                    con.Close();
+                }
+                query = String.Format("SELECT item.description, item.qty, unit.name, unit.cost FROM `line_item` item LEFT JOIN `unit` unit ON item.unit_id = unit.unit_id WHERE item.order_id = {0};", id);
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while(sdr.Read()){
+                            order.Items.Add(new LineItem
+                            {
+                                Item_Description = (sdr["Description"]).ToString(),
+                                Item_Qty = Convert.ToInt32(sdr["Qty"]),
+                                Unit_Name = (sdr["Name"]).ToString(),
+                                Unit_Cost = Convert.ToDecimal(sdr["Cost"])
+                            });
+
+                        }
+                    }
+                    con.Close();
+                }
+                foreach(LineItem item in order.Items) { 
+                    order.Order_Total += item.Unit_Cost * item.Item_Qty;
+                    order.Order_Qty += item.Item_Qty;
+                }
+            }
+            
+
+
+            
+            return View(order);
         }
     }
 }
---
+++ CaseyThompson/ApplicantTest/ApplicantTest/Models/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApplicantTest.Models
+{
+    public struct LineItem
+    {
+        public string Item_Description { get; set; }
+        public int Item_Qty { get; set; }
+        public string Unit_Name { get; set; }
+        public decimal Unit_Cost { get; set; }
+    }
+    public class Order
+    {
+        public int Order_Id { get; set; }
+        public decimal Order_Total { get; set; }
+        public int Order_Qty{ get; set; }
+        public string User_FirstName { get; set; }
+        public string User_LastName { get; set; }
+        public string User_Address1 { get; set; }
+        public string User_Address2 { get; set; }
+        public string User_City { get; set; }
+        public string User_State { get; set; }
+        public string User_Zip { get; set; }
+        public List<LineItem> Items{ get; set; }
+
+    }
+}
